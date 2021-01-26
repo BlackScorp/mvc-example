db95f30c9d4c12225c6c6bfff1f21a0efa6d69ff
Blackscorp <cccpmik@gmail.com>
2021-01-26 21:23:54 +0100

    using bootstrap.php to initialize data

---
+++ bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors','On');
+session_start();
+
+/**
+ * Autoloader, nutzt _ in Klassen Namen um daraus die Ordnerstruktur herzuleiten
+ */
+spl_autoload_register(function ($className){
+    $path = __DIR__.'/src/'.str_replace(['_','\\'],'/',$className).'.php';
+    if(is_file($path)){
+        require_once $path;
+    }
+});
+
+const TEMPLATE_DIR = __DIR__.'/templates';
+
+const DATABASE = [
+    'host'=>'localhost',
+    'username'=>'root',
+    'dbname'=>'test',
+    'password'=>'',
+    'charset'=>'utf8'
+];
--- index.php
+++ index.php
@@ -1,27 +1,5 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors','On');
-session_start();
-
-/**
- * Autoloader, nutzt _ in Klassen Namen um daraus die Ordnerstruktur herzuleiten
- */
-spl_autoload_register(function ($className){
-    $path = __DIR__.'/src/'.str_replace(['_','\\'],'/',$className).'.php';
-    if(is_file($path)){
-        require_once $path;
-    }
-});
-
-const TEMPLATE_DIR = __DIR__.'/templates';
-
-const DATABASE = [
-    'host'=>'localhost',
-    'username'=>'root',
-    'dbname'=>'test',
-    'password'=>'',
-    'charset'=>'utf8'
-];
+require_once  __DIR__.'/bootstrap.php';
 
 $foo = new Controller_Index();
 echo $foo->indexAction();
